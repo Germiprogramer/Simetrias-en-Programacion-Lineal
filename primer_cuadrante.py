9d8ca3c3c827082660e526c72193ac4bcf320273
Germiprogramer <llorentemunozgerman@gmail.com>
2023-11-06 23:54:17 +0100

    commit final

---
+++ primer_cuadrante.py
@@ -0,0 +1,17 @@
+#Variante general
+
+# Importar el módulo SymPy
+import sympy as sym
+from sympy import symbols
+from sympy.plotting import plot
+
+f1 = "8-x" 
+f2 = "18-3*x"
+
+#las z son las funciones objetivo, despejamos la diversion y damos los valores que queramos a la z
+Z1 = "(13-2*x)"
+
+
+x = symbols('x')
+
+p = plot(f1, f2, Z1,(x, 0, 15))
--- segundo_cuadrante.py
+++ segundo_cuadrante.py
@@ -5,13 +5,13 @@ import sympy as sym
 from sympy import symbols
 from sympy.plotting import plot
 
-f1 = "8-x" 
-f2 = "18-3*x"
+f1 = "8+x"
+f2 = "18+3*x"
 
 #las z son las funciones objetivo, despejamos la diversion y damos los valores que queramos a la z
-Z1 = "(12-2*x)"
+Z1 = "(13+2*x)"
 
 
 x = symbols('x')
 
-p = plot(f1, f2, Z1,(x, -15, 15))
+p = plot(f1, f2, Z1,(x, -15, 0))
